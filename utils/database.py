821ba566a14d6ee66cc8061e11a9ed258bf34616
Karol-Regula <kregula@stuy.edu>
2017-05-15 12:20:15 -0400

    Added database column headings for reference.

--- utils/database.py
+++ utils/database.py
@@ -25,3 +25,15 @@ def getSampleData():
   out = c.fetchall()
   closeDB
   return out
+  
+  
+#database headings
+  
+#definitions
+#idx|Subject|Word|Definition
+
+#notes
+#idx|Subject|Form|Topic_Number|Topic_Name|SubTopic_Number|SubTopic_Name|Information_ID|Information
+
+#questions
+#ID|Test|Test Type|Year|Subject|Question Number|Part Number|Question|A|B|C|D|E|Answer|Subtopic Name|Flag|Notes
