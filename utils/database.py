e93d7485e7de32453638e2b3eb63e2ef69daf9a0
Bayless <bsmithsalzberg@stuy.edu>
2017-05-15 07:17:23 -0400

    printing subjects

--- utils/database.py
+++ utils/database.py
@@ -6,7 +6,7 @@ f = "database.db"
 
 def initializeDB():
   global c, db
-  file = 'data/database.db'
+  file = '../data/database.db'
   db = sqlite3.connect(file)
   c = db.cursor()
   #initialize.createDB()
@@ -25,3 +25,40 @@ def getSampleData():
   out = c.fetchall()
   closeDB
   return out
+
+def getSubjectsDefinitions():
+    initializeDB()
+    q = 'SELECT subject FROM definitions'
+    c.execute(q)
+    subjects = c.fetchall()
+    allSubjects = []
+    for i in subjects:
+        if not i in allSubjects:
+            allSubjects.append(i)
+    print allSubjects
+    
+def getSubjectsNotes():
+    initializeDB()
+    q = 'SELECT subject FROM notes'
+    c.execute(q)
+    subjects = c.fetchall()
+    allSubjects = []
+    for i in subjects:
+        if not i in allSubjects:
+            allSubjects.append(i)
+    print allSubjects
+    
+def getSubjectsQuestions():
+    initializeDB()
+    q = 'SELECT subject FROM questions'
+    c.execute(q)
+    subjects = c.fetchall()
+    allSubjects = []
+    for i in subjects:
+        if not i in allSubjects:
+            allSubjects.append(i)
+    print allSubjects
+
+getSubjectsDefinitions()
+getSubjectsNotes()
+getSubjectsQuestions()
