effa8e1f309da03e069a983fddace5694237ebcc
evookelj <evukelj@stuy.edu>
2017-05-16 12:22:48 -0400

    Start changing js and html to fit new format

--- static/js/tetea.js
+++ static/js/tetea.js
@@ -20,29 +20,32 @@ var setSelect = function(lastSelect,fxn,thisSelect) {
         }
 	})
 	.done(function() {
-		var topicList = topics.split(",")
-		var select = document.getElementById(thisSelect);
-		// clear old subtopics
-		while (select.hasChildNodes()) {
-			select.removeChild(select.lastChild);
-		}
-
-		var i=0;
-		for (i=0; i<topicList.length; i++){
-			var node = document.createElement("option");
-			node.setAttribute("value", topicList[i])
-			node.innerHTML = topicList[i]
-			select.appendChild(node)
-		}
+		setDropdown(topics, thisSelect);
 	});
 };
 
-// sets topics based on material type
-var setTopics = function() {
-	setSelect("selectTypes","/getTopics/","selectTopics");
-}
+var setDropdown = function(to_add, thisSelect) {
+	var topicList = topics.split(",")
+	var select = document.getElementById(thisSelect);
+	// clear old subtopics
+	while (select.hasChildNodes()) {
+		select.removeChild(select.lastChild);
+	}
 
-// sets subtopics based on topic
-var setSubtopics = function() {
-	setSelect("selectTopics","/getSubtopics/","selectSubtopics")
-}
+	var i=0;
+	for (i=0; i<topicList.length; i++){
+		var node = document.createElement("option");
+		node.setAttribute("value", topicList[i])
+		node.innerHTML = topicList[i]
+		select.appendChild(node)
+	}
+};
+
+// sets types based on subject
+var setTypes = function() {
+	setDropdown(['Questions','Notes','Lessons'], "selectTypes");
+};
+
+var setTopics = function() {
+	setSelect("selectTypes", "getSubtopics", "selectTopics");
+};
