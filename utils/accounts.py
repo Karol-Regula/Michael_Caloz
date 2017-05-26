3aa42a9adc3e1653d86b346012a3d536bc93a1b4
Karol-Regula <kregula@stuy.edu>
2017-05-26 12:41:06 -0400

    Fixed indentation in accounts.py. Use ANY password for login for now.

--- utils/accounts.py
+++ utils/accounts.py
@@ -17,114 +17,115 @@ def closeDB():
 
 #Ask Yvonne for default password
 def initializeAdmin():
-        initializeDB()
-        tableCreateQuery = "CREATE TABLE IF NOT EXISTS Accounts (username TEXT, password TEXT, salt TEXT, defaultPW TEXT)"
-        c.execute(tableCreateQuery)
-        user = "Admin"
-        adminQuery = "INSERT INTO Accounts VALUES (?,?,?,?)"
-        salt = urandom(10).encode('hex')
-        password = sha1(""+salt).hexdigest()
-	default = sha1(""+salt).hexdigest()
-	c.execute(adminQuery, (user, password, salt, default))
-	ret = "Admin account not created. Now login."
-        closeDB()
-        return ret
+  initializeDB()
+  tableCreateQuery = "CREATE TABLE IF NOT EXISTS Accounts (username TEXT, password TEXT, salt TEXT, defaultPW TEXT)"
+  c.execute(tableCreateQuery)
+  user = "Admin"
+  adminQuery = "INSERT INTO Accounts VALUES (?,?,?,?)"
+  salt = urandom(10).encode('hex')
+  password = sha1("stuff"+salt).hexdigest()
+  default = sha1("stuff"+salt).hexdigest()
+  c.execute(adminQuery, (user, password, salt, default))
+  ret = "Admin account not created. Now login."
+  closeDB()
+  return ret
 
 #print initializeAdmin()
 
 # Returns "" if login succeeds and returns
 # string of reason why login failed otherwise
 def login(user,password):
-	initializeDB()
-	query = ("SELECT * FROM Accounts WHERE username=?")
-	ret = "n/a"
-
-	try: # TABLE EXISTS
-		sel = c.execute(query,(user,))
-
-		# Check if pass matches
-		# record = (user, password, salt)
-		for record in sel:
-			password = sha1(password+record[2]).hexdigest()
-			if (password==record[1]):
-				ret = ""
-			else: #password doesn't match
-				ret = "Wrong username or password"
-
-		if ret=="n/a": # no records
-			ret = "Username does not exist"
-
-	except: # TABLE does NOT exist
-                ret = "Admin account does not exist. Please contact administator."
-
-        closeDB()
-	return ret
+  initializeDB()
+  query = ("SELECT * FROM Accounts WHERE username=?")
+  ret = "n/a"
+  
+  # try: # TABLE EXISTS
+  # 	sel = c.execute(query,(user,))
+  # 
+  # 	# Check if pass matches
+  # 	# record = (user, password, salt)
+  # 	for record in sel:
+  # 		password = sha1(password+record[2]).hexdigest()
+  # 		if (password==record[1]):
+  # 			ret = ""
+  # 		else: #password doesn't match
+  # 			ret = "Wrong username or password"
+  # 
+  # 	if ret=="n/a": # no records
+  # 		ret = "Username does not exist"
+  # 
+  # except: # TABLE does NOT exist
+  #               ret = "Admin account does not exist. Please contact administator."
+  # 
+  #       closeDB()
+  ret = ""
+  return ret
 
 #gives admin the ability to change the password
 def changePass(user, pw):
-        initializeDB()
-        query = "SELECT * FROM Accounts WHERE username=?"
-        info = c.execute(query, (user,))
-        salt = ""
-        for record in info:
-                salt = record[2]
-        pw = sha1(pw+salt).hexdigest()
-        changeQuery = "UPDATE Accounts SET PASSWORD = ? WHERE USERNAME = ?"
-        c.execute(changeQuery, (pw, user))
-        ret = "Password is now changed."
-        closeDB()
-        return ret
-
+  initializeDB()
+  query = "SELECT * FROM Accounts WHERE username=?"
+  info = c.execute(query, (user,))
+  salt = ""
+  for record in info:
+    salt = record[2]
+    pw = sha1(pw+salt).hexdigest()
+    changeQuery = "UPDATE Accounts SET PASSWORD = ? WHERE USERNAME = ?"
+    c.execute(changeQuery, (pw, user))
+    ret = "Password is now changed."
+    closeDB()
+  return ret
+  
 def getPassword(user):
-        initializeDB()
-        queryCheck = "SELECT * FROM Accounts WHERE username=?"
-        check = c.execute(queryCheck, (user,))
-        password = ""
-        for record in check:
-                password = record[1]
-        closeDB()
-        return password
+  initializeDB()
+  queryCheck = "SELECT * FROM Accounts WHERE username=?"
+  check = c.execute(queryCheck, (user,))
+  password = ""
+  for record in check:
+    password = record[1]
+    closeDB()
+  return password
 
 def getDefault(user):
-        initializeDB()
-        queryCheck = "SELECT * FROM Accounts WHERE username=?"
-        check = c.execute(queryCheck, (user,))
-        password = ""
-        for record in check:
-                password = record[3]
-        closeDB()
-        return password
+  initializeDB()
+  queryCheck = "SELECT * FROM Accounts WHERE username=?"
+  check = c.execute(queryCheck, (user,))
+  password = ""
+  for record in check:
+    password = record[3]
+    closeDB()
+  return password
 
 initializeAdmin()
-print getDefault("Admin")
-print changePass("Admin","passChanged!")
-print getPassword("Admin")
+#print getDefault("Admin")
+#print changePass("Admin","passChanged!")
+#print getPassword("Admin")
 
 # Checks if user already exists
 # Returns True if exists, False if doesn't
 def duplicate(user):
-	db = connect(f)
-	c = db.cursor()
-	query = ("SELECT * FROM Accounts WHERE username=?")
-	sel = c.execute(query,(user,))
-	ret = False
-	for record in sel:
-		# if any records, must be true
-		ret = True
-		break
-	db.commit()
-	db.close()
-	return ret
+  db = connect(f)
+  c = db.cursor()
+  query = ("SELECT * FROM Accounts WHERE username=?")
+  sel = c.execute(query,(user,))
+  ret = False
+  for record in sel:
+    # if any records, must be true
+    ret = True
+    break
+  db.commit()
+  db.close()
+  return ret
 
 # TESTS:
-	#clear accounts
+#clear accounts
 #db = connect(f)
 #c = db.cursor()
 #c.execute("DELETE FROM Accounts WHERE username=='EmmaVook'")
 #db.commit()
 #db.close()
 
-	# run actual tests
+# run actual tests
 #print login("EmmaVook", "softDev123")
 #print register("EmmaVook", "softDev123", "Bayless")
 #print register("EmmaVook", "softDev123", "softDev123")
