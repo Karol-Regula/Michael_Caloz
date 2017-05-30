57f214ff2d3407084aff789da3d7846b9709e1dd
Bayless <bsmithsalzberg@stuy.edu>
2017-05-30 07:18:51 -0400

    pushing bc i give up - has upload things

--- app.py
+++ app.py
@@ -101,18 +101,46 @@ def login():
                 return redirect("/admin")
         else:
                 return render_template("admin.html", login_error=text)
-        
-@app.route("/admin")
-def admin():
+
+
+'''def admin():
         #add something that shows data from access.db
         info = accessDB.getInfoArray()
         #app.run(info)
-        return render_template('admin.html', subjects=info);
+        return render_template('admin.html', subjects=info)'''
+@app.route("/admin", methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
 @app.route("/logout/", methods=['POST'])
 def logout():
         session.pop("username")
         return redirect("/")
+
 
 if __name__ == "__main__":
         app.debug = True
