eecad62543c678cc657803ddbe47f408b14953fa
ChanYvonne <ychan1352@gmail.com>
2017-05-31 12:35:19 -0400

    incorporated error messaging for uploading files

--- app.py
+++ app.py
@@ -121,24 +121,30 @@ return render_template('admin.html', subjects=info)'''
 
 @app.route("/admin", methods=['GET', 'POST'])
 def upload_file():
+  msg = ""
+  info = accessDB.getInfoArray()
   if request.method == 'POST':
     # check if the post request has the file part
     if 'file' not in request.files:
-      flash('No file part')
+      #flash('No file part')
       return redirect(request.url)
     file = request.files['file']
     # if user does not select file, browser also
     # submit a empty part without filename
     if file.filename == '':
-      flash('No selected file')
-      return redirect(request.url)
+      msg = "Please select a file first"
+      #flash('No selected file')
+      return render_template('admin.html', subjects = info, message=msg)
+    if '.sql' not in file.filename:
+      msg = "Only SQL files are accepted"
+      return render_template('admin.html', subjects = info, message=msg)
     else:
       filename = secure_filename(file.filename)
       print filename
       file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-      return redirect(url_for('upload_file',filename=filename))
-  info = accessDB.getInfoArray()
-  return render_template('admin.html', subjects=info)
+      print msg
+      return redirect(url_for('upload_file',filename=filename, message=msg))
+  return render_template('admin.html', subjects=info, message=msg)
 
 @app.route("/logout/", methods=['POST'])
 def logout():
