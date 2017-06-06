dc2cb842de67576934d73db4b71212921708257d
Karol-Regula <kregula@stuy.edu>
2017-06-06 00:23:01 -0400

    Made database uploading function live?

--- utils/database.py
+++ utils/database.py
@@ -335,8 +335,10 @@ def returnDefinitionLetterAmount(subject):
   
 def convertDB(filename):
   os.system('./utils/sqlToSqlite.sh uploads/' + filename +  '| sqlite3 data/databaseNEW.db')
-  #os.rename('data/database.db', 'data/databaseOLD.db'); #uncomment this to make this function live
-  os.rename('data/databaseNEW.db', 'data/database1.db'); #change database1 to database to make this function live
+  os.rename('data/database.db', 'data/databaseOLD.db'); #uncomment this to make this function live
+  os.rename('data/databaseNEW.db', 'data/database.db'); #change database1 to database to make this function live
+  os.rename('uploads/'+ filename, 'uploads/database.sql')
+  os.system('mv uploads/database.sql data/')
   #deduplicateDatabase();
   return
 
