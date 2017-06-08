bfcb59fd68a3a7dd8415f0ef4678f0bbc2fbc352
Karol-Regula <kregula@stuy.edu>
2017-06-08 12:51:52 -0400

    Another change to script running.

--- texttetea/utils/database.py
+++ texttetea/utils/database.py
@@ -382,7 +382,7 @@ def convertDB(filename):
   #rint "filename: " + str(filename)
   #print DIR
   try:
-    os.system('. ' + DIR + '/utils/sqlToSqlite.sh ' + DIR + 'uploads/' + filename +' | sqlite3 ' + DIR + 'data/databaseNEW.db')
+    os.system(DIR + './utils/sqlToSqlite.sh ' + DIR + 'uploads/' + filename +' | sqlite3 ' + DIR + 'data/databaseNEW.db')
     os.rename(DIR + 'data/database.db', DIR + 'data/databaseOLD.db'); #uncomment this to make this function live
     os.rename(DIR + 'data/databaseNEW.db', DIR + 'data/database.db'); #change database1 to database to make this function live
     os.rename(DIR + 'uploads/' + filename, DIR + 'data/database.sql')
